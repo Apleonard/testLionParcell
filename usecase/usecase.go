ac7b1b2ca3b219fb777f307f620c07c3a9bdcc3d
Leonard Adi <leonard.adi@refactory.id>
2022-11-10 16:00:02 +0700

    try with go func

--- usecase/usecase.go
+++ usecase/usecase.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"mime/multipart"
 	"strconv"
+	"sync"
 	"testLionParcell/models"
 	"testLionParcell/repository"
 	"time"
@@ -28,6 +29,8 @@ func NewUsecase(repo repository.Repositories) Usecases {
 }
 
 func (u *usecases) Upload(file multipart.FileHeader) error {
+
+	fmt.Println("masuk usecase")
 
 	f, err := file.Open()
 	if err != nil {
@@ -53,57 +56,72 @@ func (u *usecases) processUploadFile(content [][]string, file multipart.FileHead
 	success := int64(0)
 	fail := int64(0)
 
-	for i, contentData := range content[1:] {
-		for i, v := range contentData {
-			switch i {
-			case 0:
-				batchInt, err := strconv.Atoi(v)
-				if err != nil {
-					return nil
+	type Fail struct {
+		Name         string
+		ErrorMessage string
+	}
+
+	chanCreateInternalProviderFail := make(chan *Fail)
+	defer close(chanCreateInternalProviderFail)
+
+	var wg sync.WaitGroup
+
+	go func() {
+		for i, contentData := range content[1:] {
+			wg.Add(1)
+			for i, v := range contentData {
+				switch i {
+				case 0:
+					batchInt, err := strconv.Atoi(v)
+					if err != nil {
+						// return nil
+					}
+					payroll.Batch = int64(batchInt)
+				case 1:
+					payroll.AccountName = v
+				case 2:
+					payroll.AccountNumber = v
+				case 3:
+					userIDInt, err := strconv.Atoi(v)
+					if err != nil {
+						// return nil
+					}
+					payroll.UserID = int64(userIDInt)
+				case 4:
+					AmountFloat64, err := strconv.ParseFloat(v, 64)
+					if err != nil {
+						// return nil
+					}
+					payroll.Amount = AmountFloat64
+				case 5:
+					payroll.Status = v
 				}
-				payroll.Batch = int64(batchInt)
-			case 1:
-				payroll.AccountName = v
-			case 2:
-				payroll.AccountNumber = v
-			case 3:
-				userIDInt, err := strconv.Atoi(v)
-				if err != nil {
-					return nil
-				}
-				payroll.UserID = int64(userIDInt)
-			case 4:
-				AmountFloat64, err := strconv.ParseFloat(v, 64)
-				if err != nil {
-					return nil
-				}
-				payroll.Amount = AmountFloat64
-			case 5:
-				payroll.Status = v
 			}
+			err := u.repo.CheckUser(payroll)
+			if err != nil {
+				success++
+				fmt.Println("error check user excel row-", i, payroll)
+			} else {
+				fail++
+				u.repo.CreatePayroll(payroll)
+			}
+
+			//set payroll log
+			payrollLog.Batch = payroll.Batch
+			payrollLog.TotalSuccess = success
+			payrollLog.TotalFailed = fail
+			payrollLog.CreatedAt = time.Now()
+			payrollLog.UpdatedAt = time.Now()
+			payrollLog.FileName = file.Filename
 		}
-		err := u.repo.CheckUser(payroll)
+
+		err := u.repo.CreatePayrollLOg(&payrollLog)
 		if err != nil {
-			success++
-			fmt.Println("error check user excel row-", i, payroll)
-		} else {
-			fail++
-			u.repo.CreatePayroll(payroll)
+			// return nil
 		}
 
-		//set payroll log
-		payrollLog.Batch = payroll.Batch
-		payrollLog.TotalSuccess = success
-		payrollLog.TotalFailed = fail
-		payrollLog.CreatedAt = time.Now()
-		payrollLog.UpdatedAt = time.Now()
-		payrollLog.FileName = file.Filename
-	}
-
-	err := u.repo.CreatePayrollLOg(&payrollLog)
-	if err != nil {
-		return nil
-	}
+	}()
+	wg.Wait()
 
 	return payroll
 }
